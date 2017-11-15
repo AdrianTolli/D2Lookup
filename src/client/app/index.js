daf4605d9f9a0efd3c6910bd64aedc4aaa26e6c2
Adrian Tollisen <adrian.tollisen@hotmail.com>
2017-11-15 13:39:24 +0100

    commit

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {getDestinyPlayer} from './api';
+import express from 'express';
+
+var app = express();
+
+app.get('/account/:membershipType/:displayName', (req, res) => {
+  getDestinyPlayer(req.params.membershipType, req.params.displayName, (error, response, body) => {
+    let Account = JSON.parse(body);
+    console.log(Account);
+  });
+});
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
-  }
-}
+    return <p> Hello friends!</p>;
+  };
+};
 
 render(<App/>, document.getElementById('app'));
+
+app.listen(3000, function(){
+  console.log('Server is listening to 3000!')
+});
